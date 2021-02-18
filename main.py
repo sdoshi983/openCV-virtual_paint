806a31b073172407dab9dd66dc9c78e8d3538fcf
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-18 23:59:23 +0530

    Iterated through the myColors list and detected all the different color present in the list

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ cap.set(4, frameHeight)
 cap.set(10,150)
 
 # list of values of different colors, like orange, green, yellow etc:
-myColors = [[5,107,0,19,255,255],
-            [133,56,0,159,156,255],
-            [57,76,0,100,255,255],
-            [90,48,0,118,255,255]]
+myColors = [[5,107,0,19,255,255],   #orange
+            [133,56,0,159,156,255], #purple
+            [57,76,0,100,255,255]]  #green
 
-def findColor(img):
+
+def findColor(img, myColors):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # lower = np.array([h_min, s_min, v_min])
-    # upper = np.array([h_max, s_max, v_max])
-    # mask = cv2.inRange(imgHSV, lower, upper)
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        cv2.imshow(str(color[0]), mask)
 
 while True:
     success, img = cap.read()
+    findColor(img, myColors)
     cv2.imshow("Result", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
