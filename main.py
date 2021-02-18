594dafc80d452bbd3bb553a3a1c065490277d6ed
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-18 23:46:00 +0530

    FindColor function started and List of values of different colors added

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import cv2
+import numpy as np
 frameWidth = 640
 frameHeight = 480
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)
+
+# list of values of different colors, like orange, green, yellow etc:
+myColors = [[5,107,0,19,255,255],
+            [133,56,0,159,156,255],
+            [57,76,0,100,255,255],
+            [90,48,0,118,255,255]]
+
+def findColor(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # lower = np.array([h_min, s_min, v_min])
+    # upper = np.array([h_max, s_max, v_max])
+    # mask = cv2.inRange(imgHSV, lower, upper)
+
 while True:
     success, img = cap.read()
     cv2.imshow("Result", img)
