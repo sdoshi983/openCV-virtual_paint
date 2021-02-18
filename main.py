7c31e3fc597ede159a6ce1ac18df1dd18657fd07
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-19 00:05:33 +0530

    function started for getting contours of objects of different colors

--- main.py
+++ main.py
@@ -21,6 +21,22 @@ def findColor(img, myColors):
         mask = cv2.inRange(imgHSV, lower, upper)
         cv2.imshow(str(color[0]), mask)
 
+
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        if area>500:
+            #cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+
 while True:
     success, img = cap.read()
     findColor(img, myColors)
